9ec01d0fee704bbd8872fe2a5b670cfeed6513bf
ong007 <dulyawat.wi@ku.th>
2020-11-28 05:00:10 +0700

    make new polymorphism

---
+++ src/main/java/Model/SortByLastRoom.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.text.ParseException;
+
+public class SortByLastRoom implements Sortby{
+    @Override
+    public void sort(BucketItem itemlist)  {
+        for(int i = 0;i<itemlist.getBucketItem().size();i++) {
+            int minPos = i;
+            for (int j = i + 1; j < itemlist.getBucketItem().size(); j++) {
+                if(itemlist.getBucketItem().get(j).compareRoom(itemlist.getBucketItem().get(minPos))==1){
+                    minPos = j;
+                }
+            }
+            Item sortMail = itemlist.getBucketItem().get(i);
+            itemlist.getBucketItem().set(i,itemlist.getBucketItem().get(minPos));
+            itemlist.getBucketItem().set(minPos,sortMail);
+        }
+    }
+}
---
+++ src/main/java/Model/Sortby.java
@@ -0,0 +1,7 @@
+package Model;
+
+import java.text.ParseException;
+
+public interface Sortby {
+    public void sort(BucketItem itemlist) throws ParseException;
+}
